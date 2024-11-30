ae8caa5e0fd0d36373a8950f7bdc9c18a732fbc2
Luan <luandsvt@gmail.com>
2024-11-30 07:11:25 -0300

    Project started

---
+++ src/main/java/com/luandias/device_allocation_system/DeviceAllocationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luandias.device_allocation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceAllocationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceAllocationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luandias/device_allocation_system/DeviceAllocationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandias.device_allocation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceAllocationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
